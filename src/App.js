37c6db47ccc0bc8278f520f9e66c30f681efc80e
James <jamesdev4you@gmail.com>
2023-06-05 17:20:29 -0400

    intro idea

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Home from "./Primary_components/Home";
+import Options from "./Primary_components/Options";
+import Contact from "./Primary_components/Contact";
+import Final from "./Primary_components/Final";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Background from "./assets/background.jpg";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundImage: `url(${Background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+      }}
+    >
+      <Home />
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/Primary_components/Home.js
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        width: "30%",
+        height: "70%",
+        backgroundColor: "white",
+        borderRadius: "25px",
+        border: "3px solid black",
+        boxShadow: "10",
+      }}
+    >
+      hi
+    </Box>
+  );
+}
